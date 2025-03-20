67565db0af185612dcfd4452471603c30331ef39
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-03-20 21:38:31 +0500

    Static list added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,19 @@
 export default function Main() {
+    const ingredients = ['Oregano', 'Basil', 'Thyme', 'Parsley', 'Rosemary'];
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log("Form Submitted");
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get('ingredient');
+        console.log(newIngredient);
+        ingredients.push(newIngredient);
+        console.log(ingredients);
+    }
+
     return(
         <main className="container">
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     aria-label="Add Ingredient" 
                     type="text" 
@@ -10,6 +22,11 @@ export default function Main() {
                     id="ingredient" />
                 <button>Add Ingredient</button>
             </form>
+            <ul className="ingredients">
+                {ingredients?.map((ingredirent, index)=>(
+                    <li key={index}>{ingredirent}</li>
+                ))}
+            </ul>
         </main>
     )
 }
