0214d1167c76b0fb761dc5c87e9facbf50c78248
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-03-21 00:12:25 +0500

    Corrected the prop name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/chef-claude-icon.png";
 
 export default function Header() {
-    return (<header class="header">
+    return (<header className="header">
         <img src={logo} alt="Chef Claude" />
         <span>Chef Claude</span>
     </header>)
