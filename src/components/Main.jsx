c45fe6afbddb163fd5fa2b6795e252d1add1c4d6
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-03-22 19:02:52 +0500

    Centered layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,27 +3,29 @@ import { useState } from "react";
 export default function Main() {
     const [ingredients, setIngredients] = useState(['Oregano', 'Basil', 'Thyme', 'Parsley', 'Rosemary']);
 
-    function addIngredient(formData){
+    function addIngredient(formData) {
         const newIngredient = formData.get('ingredient');
-        setIngredients((prevIngredients)=>[...prevIngredients, newIngredient]);
+        setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
     }
 
-    return(
+    return (
         <main className="container">
             <form action={addIngredient} className="add-ingredient-form">
-                <input 
-                    aria-label="Add Ingredient" 
-                    type="text" 
+                <input
+                    aria-label="Add Ingredient"
+                    type="text"
                     placeholder="Add Ingredient"
-                    name="ingredient" 
+                    name="ingredient"
                     id="ingredient" />
                 <button>Add Ingredient</button>
             </form>
-            <ul className="ingredients">
-                {ingredients?.map((ingredirent, index)=>(
-                    <li key={index}>{ingredirent}</li>
-                ))}
-            </ul>
+            <section className="ingredients-section">
+                <ul className="ingredients">
+                    {ingredients?.map((ingredirent, index) => (
+                        <li key={index}>{ingredirent}</li>
+                    ))}
+                </ul>
+            </section>
         </main>
     )
 }
